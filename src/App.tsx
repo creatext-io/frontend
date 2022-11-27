5948668d04a7b6bb93256d3943bbfca856151680
arjunbharti <arjunbharti774@gmail.com>
2022-11-27 21:52:19 +0530

    refactor: toggle button for multiline

--- src/App.tsx
+++ src/App.tsx
@@ -100,10 +100,26 @@ function App() {
   return (
     <>
       <section className="p-8">
+        <h2 className="text-4xl title-scrible text-center mb-6 italic font-bold">scrible</h2>
         <div className="App max-w-3xl flex flex-col mx-auto my-0 rounded-lg bg-white">
-          <label className="multi-suggestion">
-            {/* <span>Multi line suggestions</span> */}
-            <Switch onChange={(val) => setMultiLine(val)} checked={multiLine} />
+          <label className="multi-suggestion flex items-center mt-1">
+            <span className="mr-3 text-sm">multi line suggestions</span>
+            {/* <Switch onChange={(val) => setMultiLine(val)} checked={multiLine} /> */}
+            <Switch
+              checked={multiLine}
+              onChange={(val) => setMultiLine(val)}
+              onColor="#86d3ff"
+              onHandleColor="#2693e6"
+              handleDiameter={20}
+              uncheckedIcon={false}
+              checkedIcon={false}
+              boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+              activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+              height={14}
+              width={40}
+              className="react-switch"
+              id="material-switch"
+            />
           </label>
           <ReactQuill
             placeholder="start writing something..."
