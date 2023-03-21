33bd5ac009e3da08fee28ee27475bc75c370d9a5
Vinod Karadiya <vinod@buyucoin.com>
2023-03-21 23:14:56 +0530

    landing page done

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,29 +1,30 @@
 import { PopupButton } from "@typeform/embed-react";
 import React from "react";
-import video from '../public/scrible-landing-page.mp4'
+import video from "../public/scrible-landing-page.mp4";
 
-const LandingPage = () => { 
-  return(
+const LandingPage = () => {
+  return (
     <section className="flex jusify-content items-center flex-col my-0 mx-auto">
       <div className="m-5">
-        <div className="text lg:text-7xl title-scrible text-4xl italic text-center font-bold decoration-indigo-500">scrible</div>
+        <div className="text lg:text-7xl title-scrible text-4xl italic text-center font-bold decoration-indigo-500">
+          scrible
+        </div>
       </div>
       <div className="text-intro m-2 max-w-3xl ">
-        <p className="lg:mt-4 mt-2 lg:text-xl text-lg text-gray-500 text-center">scrible makes writing fast, fun and easy. Its AI features give you the confidence to write anything you want.</p>
+        <p className="lg:mt-4 mt-2 lg:text-xl text-lg text-gray-500 text-center">
+          scrible makes writing fast, fun and easy. Its AI features give you the
+          confidence to write anything you want.
+        </p>
       </div>
       <section className="flex flex-col mx-auto rounded bg-white lg:my-8 my-5 max-w-2xl w-full drop-shadow-lg">
-       <video className="flex p-4" controls>
-        <source 
-          src={video}
-          type="video/mp4"
-        />
+        <video className="flex p-4" autoPlay={true} muted loop>
+          <source src={video} type="video/mp4" />
         </video>
       </section>
       <div className="flex mx-auto max-w-2xl justify-between mt-5">
         <a
           href="https://forms.gle/gRZhGEDQfBoirAYd9"
           className="px-3 py-4 bg-indigo-500 text-white rounded font-medium"
-          
         >
           Join the waitlist
         </a>
--- src/LandingPageNew.tsx
+++ src/LandingPageNew.tsx
@@ -1,164 +1,51 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './LandingPageNew.css'
+import "./LandingPageNew.css";
 
-const Scriblelandingpage = (props) => {
+import Logo from "./assets/Logo.svg";
+import video from "../public/scrible-landing-page.mp4";
+
+const Scriblelandingpage = () => {
   return (
-    <div>
-      <div className="scriblelandingpage-navbar-logo-left">
-        <div className="scriblelandingpage-navbar-container">
-          <div className="scriblelandingpage-navbar-content">
-            <div className="scriblelandingpage-container1">
-              <div className="scriblelandingpage-navbar-brand"></div>
-              <span className="scriblelandingpage-text">
-                <span>Scrible</span>
-              </span>
-            </div>
-            <div className="scriblelandingpage-navbar-menu">
-              <div className="scriblelandingpage-navbar-link">
-                <span className="scriblelandingpage-text02">
-                  <span>About</span>
-                </span>
-              </div>
-              <div className="scriblelandingpage-navbar-link1">
-                <span className="scriblelandingpage-text04">
-                  <span>Contact</span>
-                </span>
-              </div>
-              <div className="scriblelandingpage-navbar-link2"></div>
-              <button className="scriblelandingpage-button">
-                <span className="scriblelandingpage-text06">
-                  <span>
-                    Join now
-                    <span
-                      dangerouslySetInnerHTML={{
-                        __html: ' ',
-                      }}
-                    />
-                  </span>
-                </span>
-              </button>
-            </div>
-          </div>
+    <section className="grid-container">
+      <nav>
+        <div>
+          <img src={Logo} alt="logo" />
         </div>
-      </div>
-      <div className="scriblelandingpage-hero-heading-left">
-        <div className="scriblelandingpage-container2">
-          <div className="scriblelandingpage-container3">
-            <div className="scriblelandingpage-container4">
-              <div className="scriblelandingpage-container5">
-                <span className="scriblelandingpage-text08">
-                  <br className="scriblelandingpage-text09"></br>
-                  <span className="scriblelandingpage-text10">Writing</span>
-                  <br className="scriblelandingpage-text11"></br>
-                  <span className="scriblelandingpage-text12">Reimagined</span>
-                  <br></br>
-                  <br></br>
-                </span>
-              </div>
-            </div>
-            <span className="scriblelandingpage-text15">
-              <br></br>
-              <br></br>
-              <br></br>
-              <br></br>
-              <span>Scrible makes writing fast, fun and easy.</span>
-              <br></br>
-              <span>
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
-              </span>
-              <span>
-                It&apos;s AI features gives you the confidence
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
-              </span>
-              <br></br>
-              <span>
-                to write
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
-              </span>
-              <span>
-                anything
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
-              </span>
-              <span>you want.</span>
-              <br></br>
-              <br></br>
-              <br></br>
-            </span>
-            <a
-              href="https://scrible.page/signup"
-              autoFocus
-              className="scriblelandingpage-button1"
-            >
-              <span className="scriblelandingpage-text31">
-                <span>
-                  Try for free -&gt;
-                  <span
-                    dangerouslySetInnerHTML={{
-                      __html: ' ',
-                    }}
-                  />
-                </span>
-              </span>
-            </a>
-          </div>
+        <div className="nav-link">
+          <Link to="/about">About</Link>
+          <Link to="/Contact">Contact</Link>
+          <Link to="/signup">Join now</Link>
         </div>
-        <video
-          src="/playground_assets/scrible-landing-page%20-%20hd%201080p.mp4"
-          loop
-          muted
-          poster="/playground_assets/scrible-landing-page%20-%20hd%201080p.mp4"
-          autoPlay
-          className="scriblelandingpage-video"
-        ></video>
-      </div>
-      <div className="scriblelandingpage-footer">
-        <div className="scriblelandingpage-footerslogan">
-          <span className="scriblelandingpage-text33">
-            Become a better writer
-          </span>
+      </nav>
+      <header>
+        <div className="left">
+          <h1>
+            Writing <br />
+            Reimagined
+          </h1>
+          <h3>
+            Scrible makes writing fast, fun and easy. <br /> It s AI features
+            gives you the confidence to writeanythingyou want.
+          </h3>
         </div>
-        <div className="scriblelandingpage-actions">
-          <a
-            href="https://scrible.page/signup"
-            autoFocus
-            className="scriblelandingpage-button2"
-          >
-            <span className="scriblelandingpage-text34">
-              <span>
-                Try for free -&gt;
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
-              </span>
-            </span>
-          </a>
+        <div className="right">
+          <video autoPlay>
+            <source src={video} type="video/mp4" />
+          </video>
         </div>
-        <div className="scriblelandingpage-copyright"></div>
-        <span className="scriblelandingpage-text36">
-          <span>Copyright © 2023 scrible</span>
-        </span>
-      </div>
-    </div>
-  )
-}
+      </header>
+      <footer>
+        <span className="footertext">Become a better writer</span>
+        <Link className="join-btn" to="/signup">
+          Try for free -&gt;
+        </Link>
 
-export default Scriblelandingpage
+        <span className="copyright">Copyright © 2023 scrible</span>
+      </footer>
+    </section>
+  );
+};
+
+export default Scriblelandingpage;
